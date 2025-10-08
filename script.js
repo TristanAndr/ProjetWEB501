fdc5679ea2c316a3dd1307c92774aa74ade048bf
Donovan MOUREY <93219000+Sabritel92@users.noreply.github.com>
2025-10-08 13:42:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,12 +12,16 @@ btn.addEventListener("click", () => {
 });
  
 //--------------------------------------------------------------ANIMATION BOX-----------------------------------------------------------------------
- 
 const btn_more = document.querySelectorAll('.btn_more');
  
 btn_more.forEach(btn => {
     btn.addEventListener("click", (e) => {
         if (btn.id === e.target.id) {
+            const flous = document.querySelectorAll('.box');
+            flous.forEach(flou => {
+                flou.classList.add('blur');
+            })
+ 
             let coord_btn = btn.getBoundingClientRect();
             let tailleEcranX = window.innerWidth;
             let tailleEcranY = window.innerHeight;
@@ -30,14 +34,64 @@ btn_more.forEach(btn => {
             let boxx = document.getElementById(target);
  
             boxx.style.transform = `scale(2.2) rotateY(180deg) translate(${-moveX}px,${-moveY}px)`;
-            //boxx.style.background = "rgba(0,0,0,1)";
-            //boxx.style.zIndex = "1";
+            boxx.style.background = "white"
+            boxx.style.zIndex = "1";
+            boxx.style.overflow = "scroll"
+            boxx.style.scrollbarWidth = "hidden"
+            boxx.classList.remove('blur');
  
-            valid = false;
-            console.log(valid);
-            let developpe = boxx.querySelector("p");
-            developpe.classList.remove('resume');
-            developpe.classList.add('entier');
+ 
+ 
+            let developpe = boxx.querySelectorAll("p");
+            developpe.forEach(p => {
+                p.classList.remove('resume');
+                p.classList.add('entier');
+                p.style.fontSize = "12px";
+            })
+ 
+            let developpe_titre = boxx.querySelectorAll("h3");
+            developpe_titre.forEach(titre => {
+                titre.classList.add('entier');
+            })
+ 
+            const text_return = document.querySelectorAll('.entier');
+            text_return.forEach(text => {
+                text.style.transform = 'rotateY(180deg)';
+            });
+            btn.remove();
+            //btn.style.opacity = 0;
+ 
+            
+ 
         }
     })
 });
+//--------------------------------------------------------------ANIMATION RETOUR BOX-----------------------------------------------------------------------
+ 
+ 
+ 
+ 
+ 
+ 
+//----------------------------------------------------------LIMITE CARACTERE RESUME----------------------------------------------------------------
+ 
+const resumes = document.querySelectorAll('.resume');
+const stock = Array.from(resumes).map(r => r.textContent);
+const maxChars = 300;
+ 
+ 
+ 
+function resume() {
+    let i = 0;
+    resumes.forEach(resume => {
+        if (resume.textContent.length > maxChars && resume.classList.value === 'resume') {
+            resume.textContent = resume.textContent.slice(0, maxChars) + '…';
+            i = i + 1;
+        } else {
+            resume.textContent = stock[i];
+            i = i + 1;
+        }
+    });
+    setTimeout(resume, 100);
+}
+resume();
